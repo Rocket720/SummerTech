e18df31028ecde02f8d80d1a176a3d2de0adc79a
SummerTech <SummerTech@10.144.4.239>
2017-06-26 13:31:14 -0400

    maybe close to working

--- src/TicTacToe.java
+++ src/TicTacToe.java
@@ -1,30 +1,38 @@
 import java.util.Scanner;
 public class TicTacToe {
+	private static boolean moveCheck = false;
+	private static int finalX;
+	private static int finalY;
+	private static int[] coordinates = new int[2];
 
 	public static void main(String[] args) {
+		while(moveCheck != true){
+			makeIBoard();
 
-		while(1 > 0){
-			makeBoard(); 
+			pickMove();
+		}
+		moveCheck = false;
+		while(moveCheck != true){
 			makeMove();
+
+			makeBoard();
 		}
 	}
-	public static void makeBoard(){
+	public static void makeIBoard(){
 		char[][] board = new char[3][3];
 
 		for(int i = 0; i < board.length; i++){
 			for(int j = 0; j < board.length; j++){
 				board[j][i] = '*';
 			}
-			board[i][i] = '*';
 		}
 		for(int i = 0; i < board.length; i++){
 			System.out.println(board[i]);
 		}
 	}
-	public static int[] makeMove(int[] x, int[] y){
+	public static void pickMove(){
+		boolean makeMove = false;
 		Scanner val = new Scanner(System.in);
-
-		int[] coordinates = new int[2];
 
 		int xCoord = val.nextInt();
 		int yCoord = val.nextInt();
@@ -32,7 +40,9 @@ public class TicTacToe {
 		coordinates[0] = xCoord;
 		coordinates[1] = yCoord;
 
-		System.out.println("hi");
+		finalX = xCoord;
+		finalY = yCoord;
+
 		while(xCoord > 3 || yCoord > 3){
 
 			System.out.println("invalid input, try again");
@@ -50,6 +60,25 @@ public class TicTacToe {
 			System.out.println(coordinates[i]);
 
 		}
-		return(coordinates);
+		moveCheck = true;
+	}
+	public static void makeMove(){
+		finalX = coordinates[0];
+		finalY = coordinates[1];
+		
+	}
+	public static void makeBoard(){
+		char[][] board = new char[3][3];
+
+		for(int i = 0; i < board.length; i++){
+			for(int j = 0; j < board.length; j++){
+				board[j][i] = '*';
+			}
+		}
+		for(int i = 0; i < board.length; i++){
+			System.out.println(board[i]);
+		}
+		board[coordinates[0]][coordinates[1]] = 'o';
+		moveCheck = true;
 	}
 }
